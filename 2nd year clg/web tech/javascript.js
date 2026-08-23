b309863b88b5756174cefd3427bdfc9a0247277b
imran <immui1413@gmail.com>
2026-08-24 00:44:47 +0530

    learning variables

---
+++ 2nd year clg/web tech/javascript.js
@@ -0,0 +1,14 @@
+let name = "imran";
+let age = 19;
+let course = "BCA";
+let isstudent = true;
+let clg = "GFGC";
+let gf;
+
+console.log(typeof name);
+console.log(name)
+console.log(age);
+console.log(course);
+console.log(typeof course);
+console.log(typeof age);
+console.log(isstudent);
